23737c7b5c2d8bc6c20585d52ec5c39078948145
D3Turquoise <anastasija_toleva@yahoo.com>
2023-03-10 21:38:31 +0000

    debug

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,13 +8,13 @@ function Header () {
     return (
         <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/#/">My Portfolio</Navbar.Brand>
+        <Navbar.Brand href="/react-project/#/">My Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/#/">About</Nav.Link>
-            <Nav.Link href="/#/projects">Projects</Nav.Link>
-            <Nav.Link href="/#/contact">Contact</Nav.Link>
+            <Nav.Link href="/react-project/#/">About</Nav.Link>
+            <Nav.Link href="/react-project/#/projects">Projects</Nav.Link>
+            <Nav.Link href="/react-project/#/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
