5b9e941eea2fecd4eee86f1d3ad0548c6e072fd8
D3Turquoise <anastasija_toleva@yahoo.com>
2023-03-10 21:31:59 +0000

    # routing

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,13 +8,13 @@ function Header () {
     return (
         <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">My Portfolio</Navbar.Brand>
+        <Navbar.Brand href="/#/">My Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">About</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/#/">About</Nav.Link>
+            <Nav.Link href="/#/projects">Projects</Nav.Link>
+            <Nav.Link href="/#/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
